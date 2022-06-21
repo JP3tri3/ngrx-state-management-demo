f785ceefbd21be17f5c88a318299e3a2c3cbf824
JP3tri3 <JP3tri3@gmail.com>
2022-06-21 18:03:01 -0400

    removed typo

--- src/app/state/collection.reducer.ts
+++ src/app/state/collection.reducer.ts
@@ -6,7 +6,6 @@ export const initialState: ReadonlyArray<string> = [];
 export const collectionReducer = createReducer(
   initialState,
   on(removeBook, (state, { bookId }) => state.filter((id) => id !== bookId)),
-  on(addBook, (state, { bookId }) => state.filter((id) => id !== bookId)),
   on(addBook, (state, { bookId }) => {
     if (state.indexOf(bookId) > -1) return state;
     return [...state, bookId];
